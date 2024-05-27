5c46f88c0d971c86f6d52d0074f533ba30a49675
lum8rjack <4932619+lum8rjack@users.noreply.github.com>
2024-05-27 12:17:47 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # caddy-ja4h
-Caddy v2 module to calculate the JA4H fingerprinter for each request
+
+Caddy v2 module to calculate the JA4H fingerprinter for each request and add it to the headers.
+
+## Installation
+
+You can build Caddy by yourself by installing [xcaddy](https://github.com/caddyserver/xcaddy) and running:
+```bash
+xcaddy build --with github.com/lum8rjack/caddy-ja4h
+```
+
+If you want to clone and make any changes, you can test locally with the following command:
+```bash
+# Specify the location of the local build
+ xcaddy build --with github.com/lum8rjack/caddy-ja4h=./caddy-ja4h
+```
+
+### Caddyfile
+
+Below is an example Caddyfile that will calculate the JA4H fingerprint for each request and add it as a new header before forwarding the request on.
+```
+{
+  admin off
+  order ja4h_header first
+}
+
+https://test.example.com {
+  ja4h_header
+
+  handle /* {
+    reverse_proxy https://localhost:8080 {
+      header_up Host {http.request.host}
+      transport http {
+        tls
+        tls_insecure_skip_verify
+      }
+    }
+  }
+}
+```
+
+
+## References
+
+- [JA4+ Network Fingerprinting](https://blog.foxio.io/ja4+-network-fingerprinting)
+- [ja4 GitHub](https://github.com/FoxIO-LLC/ja4)
+- [go-ja4h](https://github.com/lum8rjack/go-ja4h)
---
+++ main.go
@@ -0,0 +1,65 @@
+package caddy_ja4h
+
+import (
+	"net/http"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	ja4h "github.com/lum8rjack/go-ja4h"
+)
+
+func init() {
+	caddy.RegisterModule(Middleware{})
+	httpcaddyfile.RegisterHandlerDirective("ja4h_header", parseCaddyfile)
+}
+
+// Middleware implements an HTTP handler that calculates the
+// JA4H fingerprint and adds it as a header.
+type Middleware struct{}
+
+// CaddyModule returns the Caddy module information.
+func (Middleware) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.ja4h_header",
+		New: func() caddy.Module { return new(Middleware) },
+	}
+}
+
+// Provision implements caddy.Provisioner.
+func (m *Middleware) Provision(ctx caddy.Context) error {
+	return nil
+}
+
+// Validate implements caddy.Validator.
+func (m *Middleware) Validate() error {
+	return nil
+}
+
+// ServeHTTP implements caddyhttp.MiddlewareHandler.
+func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	j := ja4h.JA4H(r)
+	r.Header.Add("JA4H", j)
+	return next.ServeHTTP(w, r)
+}
+
+// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
+func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	return nil
+}
+
+// parseCaddyfile unmarshals tokens from h into a new Middleware.
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m Middleware
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+	return m, err
+}
+
+// Interface guards
+var (
+	_ caddy.Provisioner           = (*Middleware)(nil)
+	_ caddy.Validator             = (*Middleware)(nil)
+	_ caddyhttp.MiddlewareHandler = (*Middleware)(nil)
+	_ caddyfile.Unmarshaler       = (*Middleware)(nil)
+)
